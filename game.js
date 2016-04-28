f03420589abfadfc59ec9a79cafeaaf06d327500
Anton <tzat.anton@gmail.com>
2016-04-28 20:23:48 +0300

    rename game

--- game.js
+++ game.js
@@ -1,35 +1,67 @@
-var game = {};
-var canvas = document.getElementById('canvasid');
-var context = canvas.getContext('2d');
+"use strict";
 
-var objects =[];
-var draw3D={};
-draw3D.draw=function () {
+var position = 0;
+var faces = [];
 
+var rotationMap = {"up":"bottom","down":"top","left":"right","right":"left"};
+
+
+window.addEventListener("keydown",function (args) {
+	if(args.keyCode == 38 || args.keyCode == 87)move("up");
+	if(args.keyCode == 39 || args.keyCode == 68)move("right");
+	if(args.keyCode == 40 || args.keyCode == 83)move("down");
+	if(args.keyCode == 37 || args.keyCode == 65)move("left");
+})
+
+window.onload = function() {
+	var first = new Face(undefined,function (cnv,cnt) {
+		var width = cnv.width;
+		var height = cnv.height;
+		cnt.fillStyle="rgb(248,237,240)";
+		cnt.fillRect(0,0,width,height);
+	});
+	faces.push(first);
+	first.load();
+	draw();
 }
 
-game.init = function () {
-  canvas.width = 800;
-  canvas.height = 800;
+function Face(next,fn){
+	this.next = {};
+	if (next == undefined){
+		this.next.up = this;
+	}
 
-  var cube = {};
-  cube.size = 70;
-  cube.vtx = []
-  cube.lines =[];
-  for (var a = 0; a < 1; a++) {
-    for (var b = 0; b < 1; b++) {
-      for (var c = 0; c < 1; c++) {
-        cube.vtx.push(-50+a*100,-50+b*100,-50+c*100);
-      }
-      cube.lines.push([cube.vtx[length-1],cube.vtx[length-2]])
-    }
-  }
+	this.load = function () {
+		this.canvas = document.createElement('canvas');
+		this.canvas.width = 200;
+		this.canvas.heght = 200;
+		//this.canvas.className = "basicFace";
+		this.context = this.canvas.getContext('2d');
+		this.draw(this.canvas,this.context);
+
+		this.clear = function () {
+			this.context
+		}
+	}
+	this.next = next;
+	this.draw = fn;
 }
-game.draw = function () {
-  context.clearRect(0,0,canvas.width,canvas.height);
-  for (int a=0;a<objects.length;a++){
-    objects.draw();
-  }
-  context.beginPath();
-  requestAnimationFrame(game.draw);
+
+function move(dir) {
+	if(faces[position][dir] != undefined){
+		faces[position].canvas.className=rotationMap[dir];
+		faces[position].next[dir].canvas.className="front";
+	}
+}
+
+
+function drawTest(cont) {
+	context.strokeStyle = "#FF0000";
+	context.beginPath();
+	context.moveTo(10,10);
+	context.lineTo(100,100);
+	context.stroke();
+}
+function draw() {
+
 }
