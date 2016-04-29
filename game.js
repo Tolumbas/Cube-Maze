82bcd3f4d96e5c71827c0edc292ed66a34799309
Anton <tzat.anton@gmail.com>
2016-04-29 12:11:10 +0300

    made cube work, but it's a bit cluncy

--- game.js
+++ game.js
@@ -1,18 +1,19 @@
 "use strict";
 
 var front;
-var rotationMap = {"up":"bottom","down":"top","left":"right","right":"left"};
+var rotationMap = {"up":"top","down":"bottom","left":"left","right":"right"};
+var oppositeRotationMap = {"top":"bottom","bottom":"top","left":"right","right":"left"};
 var gameContainer = document.getElementById('game');
 
 window.addEventListener("keydown",function (args) {
-	if(args.keyCode == 38 || args.keyCode == 87){if(front.next.up)appendToFront(front.next.up,"bottom");return;}
-	if(args.keyCode == 39 || args.keyCode == 68){if(front.next.left)appendToFront(front.next.left,"right");return;}
-	if(args.keyCode == 40 || args.keyCode == 83){if(front.next.down)appendToFront(front.next.down,"top");return;}
-	if(args.keyCode == 37 || args.keyCode == 65){if(front.next.left)appendToFront(front.next.left,"left");return;}
+	if(args.keyCode == 38 || args.keyCode == 87){if(front.face.next.up)appendToFront(front.face.next.up,"bottom");return;}
+	if(args.keyCode == 39 || args.keyCode == 68){if(front.face.next.right)appendToFront(front.face.next.right,"right");return;}
+	if(args.keyCode == 40 || args.keyCode == 83){if(front.face.next.down)appendToFront(front.face.next.down,"top");return;}
+	if(args.keyCode == 37 || args.keyCode == 65){if(front.face.next.left)appendToFront(front.face.next.left,"left");return;}
 })
 
 window.onload = function() {
-	var first = function (cnv,cnt) {
+	window.first = function (cnv,cnt) {
 		var width = cnv.width;
 		var height = cnv.height;
 		cnt.fillStyle="rgb(248,237,240)";
@@ -20,35 +21,55 @@ window.onload = function() {
 	};
 	first.next = {};
 	first.next.up = first;
+	first.next.left = first;
+	first.next.right = first;
+	first.next.down = first;
 	appendToFront(first);
 }
 
 function removeFromContainer(face) {
 		gameContainer.removeChild(face);
 }
+/*
+function loadNext(face){
+	if (face == undefined || face.next == undefined)return;
+	for (var f in face.next){
+		var cnv = document.createElement("canvas");
+		cnv.className = rotationMap[f];
+		var ctx = cnv.getContext("2d");
+		face.next[f](cnv,ctx);
+		gameContainer.appendChild(cnv);
+	}
+}*/
+
 function appendToFront(face,dir) {
 	if (face == undefined)return;
 	if (front == undefined){
-		for (var a in front.next){
 			var cnv = document.createElement("canvas");
 			var ctx = cnv.getContext("2d");
 			face(cnv,ctx);
 			cnv.className = "front";
-		}
 
-		var cnv = document.createElement("canvas");
-		var ctx = cnv.getContext("2d");
-		face(cnv,ctx);
-		cnv.className = "front";
-		front = cnv;
-		front.next = face.next;
-		gameContainer.appendChild(cnv);
-		return;
+			front = cnv;
+			front.face = face;
+
+			gameContainer.appendChild(front);
+			return;
 	}
 	front.className=dir;
-	setTimeout(removeFromContainer,500,front);
-	front.next.className = "front";
-	front = front.next;
+	setTimeout(removeFromContainer,200,front);
+
+	var cnv = document.createElement("canvas");
+	var ctx = cnv.getContext("2d");
+	face(cnv,ctx);
+	cnv.className = oppositeRotationMap[dir];
+	gameContainer.appendChild(cnv);
+	setTimeout(function () {
+		cnv.className = "front";
+	},10);
+
+	front = cnv;
+	front.face = face;
 }
 
 
